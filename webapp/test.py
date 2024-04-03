2419156266b7fe7a7c6d5e57709c7e43bb53108e
ManvithLB <76672571+ManvithLB@users.noreply.github.com>
2024-04-03 23:05:21 +0530

    Delete webapp directory

--- webapp/test.py
+++
@@ -1,9 +0,0 @@
-import spacy
-from spacy.scorer import Scorer
-
-# Default scoring pipeline
-scorer = Scorer()
-
-# Provided scoring pipeline
-nlp = spacy.load("en_core_web_sm")
-scorer = Scorer(nlp)
